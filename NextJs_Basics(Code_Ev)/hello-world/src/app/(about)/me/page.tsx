a52d994725c5e65e761b3e2d214cc5f5be373f5c
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-15 14:03:40 +0545

    docs: metadata api

--- NextJs_Basics(Code_Ev)/hello-world/src/app/(about)/me/page.tsx
+++ NextJs_Basics(Code_Ev)/hello-world/src/app/(about)/me/page.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
+import { type Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Birat Title",
+  description: "This is my page description",
+  keywords: ["page2", "page3", "page4"],
+  twitter: {
+    card: "summary_large_image",
+    title: "My Page Title",
+    description: "This is my page description",
+    images: ["https://www.example.com/my-image.jpg"],
+  },
+};
+
 const Me = () => {
   return <div>Me</div>;
 };
