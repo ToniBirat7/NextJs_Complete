fc3cc9581661749a0dafdbfbec5f53aecce050bb
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-17 17:00:58 +0545

    docs: loading in nextjs

--- NextJs_Basics(Code_Ev)/hello-world/src/app/(docs)/error.tsx
+++ NextJs_Basics(Code_Ev)/hello-world/src/app/(docs)/error.tsx
@@ -1,8 +1,21 @@
 "use client";
 import React from "react";
+import { useRouter } from "next/router";
+import { startTransition } from "react";
 
-const Error = ({ error }: { error: Error }) => {
-  return <div>Error Encounterd, {error.message}</div>;
+const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
+  return (
+    <>
+      <div>Error Encounterd, {error.message}</div>
+      <button
+        onClick={() => {
+          reset();
+        }}
+      >
+        Try Again
+      </button>
+    </>
+  );
 };
 
 export default Error;
