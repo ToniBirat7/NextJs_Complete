961d38b72766d007e14cb4cc2c3cce1fbc8695e1
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-17 16:34:29 +0545

    docs: loading in nextjs

--- NextJs_Basics(Code_Ev)/hello-world/src/app/(docs)/doc/page.tsx
+++ NextJs_Basics(Code_Ev)/hello-world/src/app/(docs)/doc/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
+function getRandInt(num: number) {
+  return Math.floor(Math.random() * num);
+}
+
 const Docs = async () => {
   await new Promise((resolve) => {
     setTimeout(() => {
       resolve("Compelted");
     }, 3000);
   });
+  const rand = getRandInt(2);
+  if (rand === 1) {
+    throw new Error("1 is Error");
+  }
   return <div>Docs</div>;
 };
 
