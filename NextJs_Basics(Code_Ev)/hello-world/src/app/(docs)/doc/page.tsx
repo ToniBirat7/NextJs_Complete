ab702817d7cae38918f061c7d4d8f6bd08e46e54
ToniBirat7 <gotofreelancing1@gmail.com>
2025-10-17 16:22:07 +0545

    docs: loading in nextjs

--- NextJs_Basics(Code_Ev)/hello-world/src/app/(docs)/doc/page.tsx
+++ NextJs_Basics(Code_Ev)/hello-world/src/app/(docs)/doc/page.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 
 const Docs = async () => {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Delay");
-    }, 2000);
-  });
+  useEffect(() => {
+    for (let i = 0; i < 9999; i++) {
+      console.log("i", i);
+    }
+    return () => {
+      console.log("UnMounted");
+    };
+  }, []);
   return <div>Did Loading Appear?</div>;
 };
 
